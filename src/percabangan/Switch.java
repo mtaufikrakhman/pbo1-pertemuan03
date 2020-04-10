285e82736ff478ebfcff393e839d2d09d2f81f89
mtaufikrakhman <62371800+mtaufikrakhman@users.noreply.github.com>
2020-04-10 19:33:47 +0700

    switch simple

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case 1:
+                System.out.println("urang joging");
+                break;
+            case 2:
+                System.out.println("urang besapida");
+                break;
+            case 3:
+                System.out.println("urang bejalan wara");
+                break;
+            case 4:
+                System.out.println("urang urang bedua laki bini");
+                break;
+            default:
+                System.out.println("kuntilanak");
+                break;
+        }
+    }
+}
