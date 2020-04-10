2470f20a398e263d94f06abcc583b131e2ddfe4a
mtaufikrakhman <62371800+mtaufikrakhman@users.noreply.github.com>
2020-04-10 18:49:28 +0700

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "M. TAUFIK RAKHMAN";
+    int jumlahSks = 24;
+    double indeksPresentasiKumulatif = 3.89;
+
+
+}
