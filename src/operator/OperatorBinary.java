4c8dc87749ca505d2abab245ca8076d01a21c692
mtaufikrakhman <62371800+mtaufikrakhman@users.noreply.github.com>
2020-04-10 20:09:42 +0700

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -10,6 +10,16 @@ public class OperatorBinary {
     int jumlahSks = 24;
     double indeksPresentasiKumulatif = 3.89;
 
-    
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+
+
+    }
 
 }
