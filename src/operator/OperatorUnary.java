360112f669e6c82c5aa560efd1eff132d47a937a
mtaufikrakhman <62371800+mtaufikrakhman@users.noreply.github.com>
2020-04-10 20:13:19 +0700

    Operator Unary

--- src/operator/OperatorUnary.java
+++ src/operator/OperatorUnary.java
@@ -4,7 +4,7 @@ public class OperatorUnary {
     public static void main(String[] args) {
         int i = 0;
         i++;                        //i tadinya 0 ditambahkan 1 sehingga i = 1
-        System.out.println(i);      //betulkan i nilainya 1
+        System.out.println(i);      //betulkan i nilainya jadi 1
         i++;                        //karena i tadinya sudah bernilai 1 maka sekarang lagi menjadi 2
         System.out.println(i);      //tuh betulkan nilainya 2
         i=i+1;                      //bisa dilihat i++ sama dengan i = i+1
